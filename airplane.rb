556a41ddbbe55662abf65d07e08271c32eb02940
Yury Ulanov <ulanov.yg@gmail.com>
2012-03-28 11:01:24 +0400

    Добавил класс Airplane,в котором определил переменные

---
+++ airplane.rb
@@ -0,0 +1,7 @@
+class Airplane
+  attr_reader :aircraft_type, :weight 
+  attr_accessor :speed, :altitude, :course
+  @position_x
+  @position_y 
+
+end
---
+++ init.rb
@@ -0,0 +1,7 @@
+require_relative "airplane.rb"
+
+plane1 = Airplane.new
+plane1.speed = "Airobus"
+plane1.altitude = 54
+
+puts "Aircraft type of planeq is   #{plane1.speed}"
